15a5b50e077c1d9b177d5a2cf94ccf6a0e92cb72
Karol Vidlár <karolko.vidlar@gmail.com>
2026-04-20 13:10:09 +0200

    bubble+select_sort

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from sorting import selection_sort
 from sorting import random_numbers
+from sorting import bubble_sort
 print(selection_sort([5, 1, 4, 2, 8]))
 print(selection_sort(random_numbers(20)))
+print(bubble_sort([5, 1, 4, 2, 8]))
+print(bubble_sort(random_numbers(20)))
--- sorting.py
+++ sorting.py
@@ -1,5 +1,5 @@
 import random
-
+import matplotlib.pyplot as plt
 from numpy.ma.core import minimum
 
 
@@ -20,4 +20,26 @@ def selection_sort(list_on_nums):
 
     return numbers
 
-print(selection_sort([5, 1, 4, 2, 8]))
+def bubble_sort(list_on_nums):
+    numbers = list_on_nums.copy()
+    plt.ion()
+    plt.show()
+    for _ in range(len(numbers)):
+        for i in range(len(numbers)):
+            try:
+                if numbers[i] > numbers[i+1]:
+                    numbers[i], numbers[i+1] = numbers[i+1], numbers[i]
+                    index_highlight1 = i
+                    index_highlight2 = i + 1
+                    colors = ["steelblue"] * len(numbers)
+                    colors[index_highlight1] = "tomato"
+                    colors[index_highlight2] = "tomato"
+                    plt.clf()
+                    plt.bar(range(len(numbers)), numbers, color=colors)
+                    plt.title("Bubble Sort")
+                    plt.pause(0.1)
+            except:
+                continue
+    plt.ioff()
+    plt.show()
+    return numbers
